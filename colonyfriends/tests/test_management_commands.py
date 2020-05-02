8b0d05e16fa8df5cb255b79091519ce031891149
Brett Matthews <brett_matthews@live.com.au>
2020-05-02 15:31:08 +1000

    test init people
    Signed-off-by: Brett Matthews <brett_matthews@live.com.au>

--- colonyfriends/tests/test_management_commands.py
+++ colonyfriends/tests/test_management_commands.py
@@ -4,7 +4,9 @@ from unittest.mock import patch
 from io import StringIO
 from django.core.management import call_command
 from django.core.management.base import CommandError
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+
+from colonyfriends.models import Food, Person
 
 
 class InitCompaniesTest(TestCase):
@@ -36,3 +38,184 @@ class InitCompaniesTest(TestCase):
 
         self.mock_json_load.return_value = [{'index': 0, 'bad_key': 'fake1'}, {'index': 1, 'bad_key': 'fake2'}]
         self.assertRaises(CommandError, call_command, 'init_companies')
+
+
+class InitPeopleTest(TransactionTestCase):
+
+    def setUp(self):
+
+        mock_open = patch('colonyfriends.management.commands.init_people.open')
+        self.mock_open = mock_open.start()
+        self.addCleanup(mock_open.stop)
+
+        mock_json_load = patch('colonyfriends.management.commands.init_people.json.load')
+        self.mock_json_load = mock_json_load.start()
+        self.addCleanup(mock_json_load.stop)
+
+        self.valid_payload = [
+            {
+                "_id": "595eeb9b96d80a5bc7afb106",
+                "index": 0,
+                "guid": "5e71dc5d-61c0-4f3b-8b92-d77310c7fa43",
+                "has_died": True,
+                "balance": "$2,418.59",
+                "picture": "http://placehold.it/32x32",
+                "age": 61,
+                "eyeColor": "blue",
+                "name": "Carmella Lambert",
+                "gender": "female",
+                "company_id": 58,
+                "email": "carmellalambert@earthmark.com",
+                "phone": "+1 (910) 567-3630",
+                "address": "628 Sumner Place, Sperryville, American Samoa, 9819",
+                "about": "Non duis t et aliquip.\r\n",
+                "registered": "2016-07-13T12:29:07 -10:00",
+                "tags": [
+                    "id",
+                    "quis",
+                    "ullamco",
+                    "consequat",
+                    "laborum",
+                    "sint",
+                    "velit"
+                ],
+                "friends": [
+                    {
+                        "index": 1
+                    }
+                ],
+                "greeting": "Hello, Carmella Lambert! You have 6 unread messages.",
+                "favouriteFood": [
+                    "orange",
+                    "apple",
+                    "celery"
+                ]
+            },
+            {
+                "_id": "595eeb9b1e0d8942524c98ad",
+                "index": 1,
+                "guid": "b057bb65-e335-450e-b6d2-d4cc859ff6cc",
+                "has_died": False,
+                "balance": "$1,562.58",
+                "picture": "http://placehold.it/32x32",
+                "age": 60,
+                "eyeColor": "brown",
+                "name": "Decker Mckenzie",
+                "gender": "male",
+                "company_id": 98,
+                "email": "deckermckenzie@earthmark.com",
+                "phone": "+1 (893) 587-3311",
+                "address": "492 Stockton Street, Lawrence, Guam, 4854",
+                "about": "Consecteia et.\r\n",
+                "registered": "2017-06-25T10:03:49 -10:00",
+                "tags": [
+                    "veniam",
+                    "irure",
+                    "mollit",
+                    "sunt",
+                    "amet",
+                    "fugiat",
+                    "ex"
+                ],
+                "friends": [
+                    {
+                        "index": 0
+                    },
+                    {
+                        "index": 1
+                    },
+                ],
+                "greeting": "Hello, Decker Mckenzie! You have 2 unread messages.",
+                "favouriteFood": [
+                    "carrot",
+                    "celery"
+                ]
+            },
+        ]
+
+    def test_command_success(self):
+
+        self.mock_json_load.return_value = self.valid_payload
+
+        out = StringIO()
+        call_command('init_people', stdout=out)
+        self.assertIn('Successfully Initialised People', out.getvalue())
+
+    def test_invalid_friend_index_blocks_transaction(self):
+
+        valid_payload = self.valid_payload
+
+        valid_payload.append({
+                "_id": "595eeb9b1e0d8942524c98ad",
+                "index": 2,
+                "guid": "b057bb65-e335-450e-b6d2-d4cc859ff6cc",
+                "has_died": False,
+                "balance": "$1,562.58",
+                "picture": "http://placehold.it/32x32",
+                "age": 60,
+                "eyeColor": "brown",
+                "name": "Decker Mckenzie",
+                "gender": "male",
+                "company_id": 98,
+                "email": "deckermckenzie@earthmark.com",
+                "phone": "+1 (893) 587-3311",
+                "address": "492 Stockton Street, Lawrence, Guam, 4854",
+                "about": "Consecteia et.\r\n",
+                "registered": "2017-06-25T10:03:49 -10:00",
+                "tags": [
+                    "veniam",
+                    "irure",
+                    "mollit",
+                    "sunt",
+                    "amet",
+                    "fugiat",
+                    "ex"
+                ],
+                "friends": [
+                    {
+                        "index": 50
+                    },
+                ],
+                "greeting": "Hello, Decker Mckenzie! You have 2 unread messages.",
+                "favouriteFood": [
+                    "carrot",
+                    "celery"
+                ]
+            }
+        )
+
+        self.mock_json_load.return_value = valid_payload
+
+        self.assertRaises(CommandError, call_command, 'init_people')
+
+        self.assertEqual(Person.objects.all().count(), 0)
+
+    def test_integration_test(self):
+
+        self.mock_json_load.return_value = self.valid_payload
+
+        call_command('init_people')
+
+        person0 = Person.objects.get(id=0)
+
+        self.assertEqual(
+            person0.name,
+            self.valid_payload[0]['name']
+        )
+
+        foods = []
+        for f in person0.favourite_foods.all():
+            foods.append(f.name)
+        self.assertEqual(
+            foods,
+            self.valid_payload[0]['favouriteFood']
+        )
+
+        person1 = Person.objects.get(id=1)
+
+        self.assertEqual(
+            person1.friends.all().count(),
+            1
+        )
+
+        self.assertEqual(Food.objects.all().count(), 4)
