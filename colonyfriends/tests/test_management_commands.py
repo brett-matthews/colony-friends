ec49e8d879fe8f496b683a88f6663d4e9b81df9f
Brett Matthews <brett_matthews@live.com.au>
2020-05-02 10:48:19 +1000

    test management commands
    Signed-off-by: Brett Matthews <brett_matthews@live.com.au>

---
+++ colonyfriends/tests/test_management_commands.py
@@ -0,0 +1,32 @@
+from unittest.mock import patch
+
+from io import StringIO
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.test import TestCase
+
+
+class InitCompaniesTest(TestCase):
+
+    def setUp(self):
+
+        mock_open = patch('colonyfriends.management.commands.init_companies.open')
+        self.mock_open = mock_open.start()
+        self.addCleanup(mock_open.stop)
+
+        mock_json_load = patch('colonyfriends.management.commands.init_companies.json.load')
+        self.mock_json_load = mock_json_load.start()
+        self.addCleanup(mock_json_load.stop)
+
+    def test_command_success(self):
+
+        self.mock_json_load.return_value = [{'index': 0, 'company': 'fake1'}, {'index': 1, 'company': 'fake2'}]
+
+        out = StringIO()
+        call_command('init_companies', stdout=out)
+        self.assertIn('Successfully Initialised Companies', out.getvalue())
+
+    def test_command_failure_invalid_input(self):
+
+        self.mock_json_load.return_value = [{'index': 0, 'bad_key': 'fake1'}, {'index': 1, 'bad_key': 'fake2'}]
+        self.assertRaises(CommandError, call_command, 'init_companies')
