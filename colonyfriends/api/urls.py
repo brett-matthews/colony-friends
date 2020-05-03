6c2731d3371a53d3da3ad33e1f81913f5881d4d6
Brett Matthews <brett_matthews@live.com.au>
2020-05-03 15:59:59 +1000

    add people view url
    Signed-off-by: Brett Matthews <brett_matthews@live.com.au>

--- colonyfriends/api/urls.py
+++ colonyfriends/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from colonyfriends.api.views import CompanyEmployeeListView, PeopleCommonFriendsView
+from colonyfriends.api.views import CompanyEmployeeListView, PeopleCommonFriendsView, PeopleView
 
 
 urlpatterns = [
     path('company/<int:company_id>/employees/', CompanyEmployeeListView.as_view(), name='company-employees'),
     path('people/common-friends/', PeopleCommonFriendsView.as_view(), name='people-common-friends'),
+    path('people/<int:pk>/', PeopleView.as_view(), name='people'),
 ]
